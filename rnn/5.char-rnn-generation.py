85e5c2553939e974ef190776d07a45b03b9df16e
zhoumingjun <zhoumingjun@gmail.com>
2018-09-11 10:30:32 +0800

    update

--- rnn/5.char-rnn-generation.py
+++ rnn/5.char-rnn-generation.py
@@ -1,5 +1,3 @@
-import glob
-import os
 from torch.utils.data import Dataset, DataLoader
 from os import path
 import unicodedata
@@ -7,27 +5,26 @@ import torch
 import string
 
 import torch.nn.functional as F
-from torch.nn.utils.rnn import pad_sequence, pack_sequence, pack_padded_sequence, pad_packed_sequence
 
 all_letters = string.ascii_letters + " .,;'-"
 n_letters = len(all_letters)
 
+# ascii name to tenser
+def name2tensor(s):
+    tensor = torch.zeros(len(s), n_letters)
+    for li, letter in enumerate(s):
+        tensor[li][all_letters.index(letter)] = 1
+    return tensor
+
 # dataset
 class NamesDataset(Dataset):
-    def __init__(self, data_dir, transforms=[]):
-        self.data_dir = data_dir
-
-
+    def __init__(self, data_path, transforms=[]):
         inputs = []
-
-
-        filepath = path.join(data_dir, "English.txt")
+        filepath = path.join(data_path)
 
         with open(filepath) as f:
             lines = f.readlines()
             inputs += [line.strip() for line in lines]
-
-
 
         self.inputs = inputs
         self.transforms = transforms
@@ -36,15 +33,12 @@ class NamesDataset(Dataset):
         return len(self.inputs)
 
     def __getitem__(self, idx):
-
-        item = self.inputs[idx]
-
-
-        return item
-
+        input = self.inputs[idx]
+        for transform in self.transforms:
+            input = transform(input)
+        return input
 
 
-# transform
 class UnicodeToAscii(object):
     def __init__(self, letters):
         self.letters = letters
@@ -56,13 +50,8 @@ class UnicodeToAscii(object):
             and c in self.letters
         )
 
-# ascii name to tenser
-def name2tensor(s):
-    tensor = torch.zeros(len(s), n_letters)
-    for li, letter in enumerate(s):
-        tensor[li][all_letters.index(letter)] = 1
-    return tensor
 
+namesDataset = NamesDataset('./data/names/English.txt', transforms=[UnicodeToAscii(all_letters)])
 
 
 class NamesClassifier(torch.nn.Module):
@@ -75,20 +64,18 @@ class NamesClassifier(torch.nn.Module):
         self.lstm = torch.nn.LSTM(input_size, hidden_size, 1)
         self.linear = torch.nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
-        x, hidden = self.lstm(x)
-        x = x.squeeze()
+    def forward(self, x, hidden):
+        x, hidden = self.lstm(x, hx=hidden)
+        x = x.view(-1, hidden_size)
         x = self.linear(x)
-        return F.log_softmax(x, dim=1)
+        return F.log_softmax(x, dim=1), hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
 
 
-unicode2ascii = UnicodeToAscii(all_letters)
-
-# define dataset and dataloader
-namesDataset = NamesDataset('./data/names')
 dataloader = DataLoader(namesDataset, batch_size=1, shuffle=True, num_workers=4, drop_last=True)
-transforms = [unicode2ascii]
-# hyper parameters
+
 input_size = n_letters
 hidden_size = 50
 output_size = input_size
@@ -98,78 +85,65 @@ model = NamesClassifier(input_size, hidden_size, output_size)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 criterion = torch.nn.NLLLoss()
 
+max_length = 20
+def sample(start_letter='A'):
+    with torch.no_grad():  # no need to track history in sampling
+        input = name2tensor(start_letter).view(-1, 1, input_size)
+        output_name = start_letter
+        hidden = (torch.zeros(1, 1, hidden_size), torch.zeros(1,1,hidden_size))
 
-for epoch in range(10):
+        for i in range(max_length):
+            output, hidden = model(input,hidden)
+            topv, topi = output.topk(1)
+            topi = topi[0][0]
+            if topi == n_letters - 1:
+                break
+            else:
+                letter = all_letters[topi]
+                output_name += letter
 
-    # train
+            input = name2tensor(letter).view(-1, 1, input_size)
+
+        return output_name
+
+
+# Get multiple samples from one category and multiple starting letters
+def samples(start_letters='ABC'):
+    for start_letter in start_letters:
+        print(sample(start_letter))
+
+
+for epoch in range(20):
+
     loss_sum = 0;
     nRound = 0
     for i_batch, batch in enumerate(dataloader):
 
-        # zero
-
         inputs = batch
 
         for idx, input in enumerate(inputs):
-
-            for transform in transforms:
-                input = transform(input)
-
-            label = input[1:]+all_letters[-1]
+            label = input[1:] + all_letters[-1]
 
             optimizer.zero_grad()
-
 
             input = name2tensor(input).view(-1, 1, input_size)
             label = [all_letters.index(x) for x in label]
 
-            output = model(input)
+            output, _ = model(input,  None)
             loss = criterion(output, torch.LongTensor(label))
 
             loss.backward()
             optimizer.step()
 
             loss_sum += loss
-            nRound +=1
-
+            nRound += 1
 
     print("epoch {} i_batch {} loss {}".format(epoch, i_batch, loss_sum / nRound))
-#
-#     # validate
-#     with torch.no_grad():
-#         acc = 0
-#         for i_batch, batch in enumerate(dataloader):
-#             inputs, labels = batch
-#             # pre-process
-#             inputs = [name2tensor(name) for name in inputs]
-#
-#             inputs_length = [x.size(0) for x in inputs]
-#             _, indices_sorted = torch.sort(torch.LongTensor(inputs_length), descending=True)
-#             _, indices_restore = torch.sort(indices_sorted)
-#
-#             # sort
-#             inputs_sorted = [inputs[x] for x in indices_sorted]
-#             labels_sorted = labels[indices_sorted]
-#
-#             # pack inputs
-#             pack = pack_sequence(inputs_sorted)
-#
-#             # rnn
-#             outputs = model(pack)
-#
-#             top_v, topi = torch.topk(outputs, 1)
-#             acc += (topi.view(1, -1) == labels_sorted).sum().item()
-#
-#     print("epoch {} acc:{}/{} ".format(epoch, acc, len(namesDataset)))
-#
-# # do some preidct
-# for i_batch, batch in enumerate(dataloader):
-#     input, label = batch
-#     for idx, input in enumerate(input):
-#         lang, lang_id = predict(input)
-#         print("input {}, label {}, predict {}, result: {}".format(
-#             input, all_langs[label[idx].item()],
-#             lang,
-#             lang_id == label[idx].item()))
-#
-#     break
+    samples()
+
+
+
+
+
+
+
